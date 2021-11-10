d7208d42b6fdee33c152f520208a8280436ad72a
lonelybuddy <henryzheng92@gmail.com>
2021-11-10 00:29:34 -0600

    added sun and moon icon according to current theme

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import './App.scss';
+import { ThemeProvider } from './context/ThemeProvider';
 import { TodoProvider } from './context/TodoProvider';
 import Header from './components/Header/Header';
 import TodoForm from './components/TodoForm/TodoForm';
@@ -8,7 +9,9 @@ import TodoList from './components/TodoList/TodoList';
 const App = () => {
   return (
     <main>
-      <Header />
+      <ThemeProvider>
+        <Header />
+      </ThemeProvider>
 
       <TodoProvider>
         <TodoForm />
